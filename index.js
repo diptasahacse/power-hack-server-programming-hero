6aa17957fd1d1d7d831a16d956eae0f8dc2ade7d
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-07-01 11:23:26 +0600

    use cors, json, dotenv in index file

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_PASSWORD}@cluster0.rg8j4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello Power hack!')
